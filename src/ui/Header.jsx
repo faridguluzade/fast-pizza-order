0555d5fc077091b99355684cae96e4a8a78e2f2a
faridguluzade <tu5mhbxzt.code.edu.az>
2023-08-23 01:39:47 -0800

    setting up professional file structure & adding routes & fetching data with react router loaders

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Farid</p>
+    </header>
+  );
+}
+
+export default Header;

bd7ce082f796c05a045450c916cf07f44ef7a177
faridguluzade <tu5mhbxzt.code.edu.az>
2023-08-24 21:09:44 -0800

    making reusable updateItemQuantity component and and updating cart quantity

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,9 +1,14 @@
-import { formatCurrency } from "../../utils/helpers";
-import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
+import { getCurrentQuantity } from "./cartSlice";
+
 import DeleteItem from "./DeleteItem";
+import UpdateItemQuantity from "./UpdateItemQuantity";
+
+import { formatCurrency } from "../../utils/helpers";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
+  const currentQuantity = useSelector(getCurrentQuantity(pizzaId));
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -12,6 +17,10 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
+        <UpdateItemQuantity
+          pizzaId={pizzaId}
+          currentQuantity={currentQuantity}
+        />
         <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,6 +7,7 @@ function Button({ children, disabled, to, type, onClick }) {
   const styles = {
     primary: base + " py-3 px-4 md:px-6 md:py-4",
     small: base + " py-2 px-4 md:px-5 md:py-2.5 text-xs ",
+    round: base + " py-1 px-2.5 md:px-3.5 md:py-2 text-sm ",
     secondary:
       "inline-block text-sm rounded-full font-semibold border-2 uppercase border-stone-300 tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300   hover:text-stone-800  focus:text-stone-800  focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed py-2.5 px-4 md:px-6 md:py-3.5",
   };
